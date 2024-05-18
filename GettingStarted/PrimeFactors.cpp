20f5e5b8f910153d2e2405b40535ce0a3c8c3ea0
md-adnaan-zakee-78692 <adnaan.zakee.24146@gmail.com>
2024-05-18 18:37:23 +0530

    Added Prime Factorization

---
+++ GettingStarted/PrimeFactors.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std ;
+
+void primeFactors(int num){
+    for(int i=2; i*i<=num; i++){
+        while(num%i==0){
+            cout << i << " " ;
+            num /= i ;
+        }
+    }
+    if(num != 1) cout << num ; // T.C. O(√n * log n)
+}
+
+int main(){
+    int num ;
+    cout << "Enter a number: " ;
+    cin >> num ;
+    primeFactors(num) ;
+}
