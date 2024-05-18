cca258206c3db04f430655d916b8c196a55f2cf5
md-adnaan-zakee-78692 <adnaan.zakee.24146@gmail.com>
2024-05-18 17:40:55 +0530

    Added Perfect Square

---
+++ GettingStarted/PerfectSquare.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+bool isPerfectSquare(int num){
+    double sq = sqrt(num) ;
+    return (ceil(sq) == floor(sq)) ; //if the number is integer return true
+}
+
+int main(){
+    int num1;
+    cout << "Enter a number: " ;
+    cin >> num1;
+    int res = isPerfectSquare(num1) ;
+    cout << res ;
+}
