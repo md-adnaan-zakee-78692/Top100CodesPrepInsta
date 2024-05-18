52912c3e6e3bca66d48855d51cbd0fb3555b3a13
md-adnaan-zakee-78692 <adnaan.zakee.24146@gmail.com>
2024-05-18 19:09:45 +0530

    Added Reversing Number

---
+++ GettingStarted/ReverseNum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std ;
+
+int reverse(int num){
+    int reversed_num = 0 ;
+    while(num){
+        int ld = num % 10 ;
+        reversed_num = reversed_num * 10 + ld ;
+        num /= 10 ;
+    }
+    return reversed_num ;
+}
+
+int main(){
+    int num ;
+    cout << "Enter a number: " ;
+    cin >> num ;
+    cout << reverse(num) ;
+}
